e6b9cd87296d3678ef53a32d975c4fb3afdfbad2
SoThea-2 <theas3871@gmail.com>
2026-08-26 17:59:25 +0700

    update footer with is logic

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,9 +1,19 @@
 
 function Footer() {
+    const openHour=7
+    const closeHour=18
+    const hour = new Date().getHours()
+    const isOpen = hour > openHour && hour < closeHour
+    console.log(isOpen)
 return (
     <footer>
             <div>
-            So Thea
+                {
+                    isOpen ?
+                
+            <p>កំពុងបើកដំណើរការ</p> :
+            <p>ហាងយើងខ្ញុំបើកដំណើរការនៅចន្លោះម៉ោង​​ {openHour}:00 ដល់ម៉ោង ​{closeHour}:00  </p>
+                } 
             </div>
     </footer>
 )
